fadbf32237e64c66130025e6d5500c949aaa0b5f
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-10-30 15:16:13 +0100

    restyling

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,11 +4,13 @@ import './App.css'
 function Card({image, price, title, description}) {
   return (
     <div className="card">
-      <img src={image} alt={title} className="card-image" />
+      <div className='img-price'>
+        <img src={image} alt={title} className="card-image" />
+        <div className="card-price">${price}</div>
+      </div>
       <div className="card-body">
         <h3>{title}</h3>
-        <span className="card-price">${price}</span>
-        <p>{description}</p>
+        <p className='description-info'>{description}</p>
         <button className="card-button">Not Interested</button>
       </div>
     </div>
