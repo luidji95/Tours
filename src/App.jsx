9f953bea9396df73c455daaa06ac46aa84edf5bb
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-11-03 11:39:00 +0100

    spiner loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './Card'
-
-
-
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import Card from './Card';
 
 function App() {
   const [tours, setTours] = useState([]);
-
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true); 
       try {
         const response = await fetch('https://www.course-api.com/react-tours-project');
         const data = await response.json();
         setTours(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); 
       }
     };
     
     fetchData();
-  }, [])
- 
+  }, []);
+
   const notInterested = (id) => {
     setTours(tours.filter((tour) => tour.id !== id));
   };
@@ -33,21 +30,24 @@ function App() {
   return (
     <div className="app">
       <h1>Our Tours</h1>
-      <div className="card-container">
-        {tours.map((tour) => (
-          <Card
-            key={tour.id}
-            image={tour.image}
-            title={tour.name}
-            price={tour.price}
-            description={tour.info}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <div class="lds-dual-ring"></div>
+      ) : (
+        <div className="card-container">
+          {tours.map((tour) => (
+            <Card
+              key={tour.id}
+              image={tour.image}
+              title={tour.name}
+              price={tour.price}
+              description={tour.info}
+              notInterested={() => notInterested(tour.id)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
-
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-function Card({image, price, title, description}) {
+function Card({ image, price, title, description, notInterested }) {
+
+  const [isExtended, setExtended] = useState(false);
+
+ 
+  const getShortDescription = (text) => {
+    return text.length > 100 ? text.substring(0, 100) + '...' : text;
+  };
 
 
+  const changeDescription = () => {
+    setExtended(!isExtended);
+  };
 
   return (
     <div className="card">
-      <div className='img-price'>
+      <div className="img-price">
         <img src={image} alt={title} className="card-image" />
         <div className="card-price">${price}</div>
       </div>
+
       <div className="card-body">
         <h3>{title}</h3>
-        <p className='description-info'>{description}</p>
-        <button onClick={notInterested()} className="card-button">Not Interested</button>
+        <p className="description-info">
+          {isExtended ? description : getShortDescription(description)}
+          <span className="read-more" onClick={changeDescription}>
+            {isExtended ? "View Less" : "Read More"}
+          </span>
+        </p>
+        <button onClick={notInterested} className="card-button">
+          Not Interested
+        </button>
       </div>
     </div>
   );
